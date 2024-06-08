5f2b35b099c3466b525ca9cec16c1865f255dd5d
Darialucky <144655337+Darialucky@users.noreply.github.com>
2024-06-08 13:04:48 +0300

    1

---
+++ src/db/contacts.js
@@ -0,0 +1,32 @@
+import { model, Schema } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      match: [/\S+@\S+\.\S+/, 'is invalid'],
+      required: false,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  { timestamps: true },
+);
+
+export const Contact = model('Contact', contactSchema);
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { initMongoConnection } from './db/initMongoConnection.js';
+import { setupServer } from './server.js';
+
+const bootstrap = async () => {
+  try {
+    await initMongoConnection();
+    console.log('Application started successfully!');
+  } catch (error) {
+    console.error('Error starting application', error);
+  }
+  setupServer();
+};
+
+bootstrap();
